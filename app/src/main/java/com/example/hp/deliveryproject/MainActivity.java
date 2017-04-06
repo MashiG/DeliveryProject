205a01564deebef2b43aa951117e267c686d0303
MashiG <maheshi.gunarathne1994@gmail.com>
2017-04-06 22:48:53 +0530

    Interfces Of DeliveryProject

---
+++ app/src/main/java/com/example/hp/deliveryproject/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.hp.deliveryproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn= (Button) findViewById(R.id.button1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(i);
+
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+
+
+            }
+
+        });
+    }
+}
---
+++ app/src/main/java/com/example/hp/deliveryproject/RegisterActivity.java
@@ -0,0 +1,34 @@
+package com.example.hp.deliveryproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity {
+    private EditText et_name,et_email, et_password, et_confirmpassword  ;
+    private String name,Email,password,confirmpassword;
+    Button regbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        et_name = (EditText) findViewById(R.id.name);
+        et_email = (EditText) findViewById(R.id.Email);
+        et_password = (EditText) findViewById(R.id.Password);
+        et_confirmpassword = (EditText) findViewById(R.id.confirmpassword);
+        regbtn = (Button) findViewById(R.id.registbtn);
+        regbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                finish();
+                overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
+            }
+
+        });
+
+    }
+    }
+

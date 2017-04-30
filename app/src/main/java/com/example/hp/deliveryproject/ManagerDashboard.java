7281fb78f9d958ff07373a618968245256110c4f
murtazasmart <murtazasmart@hotmail.com>
2017-04-30 23:41:13 +0530

    Controllers being added for User layouts and a few manager layouts

---
+++ app/src/main/java/com/example/hp/deliveryproject/ManagerDashboard.java
@@ -0,0 +1,10 @@
+package com.example.hp.deliveryproject;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by murtaza on 4/30/17.
+ */
+
+public class ManagerDashboard extends AppCompatActivity {
+}

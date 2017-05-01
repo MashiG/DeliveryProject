bbbed07b5186b19ae202228dbd43de413847f029
BhagyaR94 <rathnayake.bhagya94@gmail.com>
2017-05-01 15:11:54 +0530

    Manager Dashboard Button bugs fixed

--- app/src/main/java/com/example/hp/deliveryproject/ManagerAssignDeliveryAgent.java
+++ app/src/main/java/com/example/hp/deliveryproject/ManagerAssignDeliveryAgent.java
@@ -2,12 +2,14 @@ package com.example.hp.deliveryproject;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageButton;
 
 /**
  * Created by murtaza on 4/30/17.
  */
 
 public class ManagerAssignDeliveryAgent extends AppCompatActivity {
+    ImageButton btnManageDelAgent = (ImageButton) findViewById(R.id.btnManageDelAgent);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/hp/deliveryproject/ManagerDashboard.java
+++ app/src/main/java/com/example/hp/deliveryproject/ManagerDashboard.java
@@ -5,18 +5,29 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 /**
  * Created by murtaza on 4/30/17.
  */
 
 public class ManagerDashboard extends AppCompatActivity {
-    
+
+    ImageButton btnManageDelAgent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.managerdashboard);
 
+        btnManageDelAgent = (ImageButton) findViewById(R.id.btnManageDelAgent);
+        btnManageDelAgent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent manageDelAgent = new Intent(ManagerDashboard.this,AddDeliveryPerson.class);
+                startActivity(manageDelAgent);
+            }
+        });
 
     }
 }

3e0033e7d7b3aa23a22714be92aea909398a05a0
murtazasmart <murtazasmart@hotmail.com>
2017-05-01 10:42:08 +0530

    Useraddrequestpickup, userupdateorderdelivery layouts have been sorted for responsiveness

--- app/src/main/java/com/example/hp/deliveryproject/UserDashboard.java
+++ app/src/main/java/com/example/hp/deliveryproject/UserDashboard.java
@@ -1,16 +1,33 @@
 package com.example.hp.deliveryproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 /**
  * Created by murtaza on 4/29/17.
  */
 
 public class UserDashboard extends AppCompatActivity {
+    ImageButton viewImage, accountImage, changepasswordImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.userdashboard);
+        //Access images from layout to implement action listeners
+        viewImage = (ImageButton) findViewById(R.id.userdashboardimgview);
+        accountImage = (ImageButton) findViewById(R.id.userdashboardimgaccount);
+        changepasswordImage = (ImageButton) findViewById(R.id.userdashboardimgchangepassword);
+
+        viewImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent
+            }
+        });
+
     }
 }

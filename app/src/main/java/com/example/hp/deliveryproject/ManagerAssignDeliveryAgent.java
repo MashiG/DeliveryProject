7a48e503211a6ca7b2f89fd79770021c4f2e2e29
murtazasmart <murtazasmart@hotmail.com>
2017-05-10 12:28:13 +0530

    Manager Dashboard layout formating part 1

--- app/src/main/java/com/example/hp/deliveryproject/ManagerAssignDeliveryAgent.java
+++ app/src/main/java/com/example/hp/deliveryproject/ManagerAssignDeliveryAgent.java
@@ -13,6 +13,6 @@ public class ManagerAssignDeliveryAgent extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.managerassigndeliveryagent);
+        setContentView(R.layout.managerallocatedeliveryagent);
     }
 }

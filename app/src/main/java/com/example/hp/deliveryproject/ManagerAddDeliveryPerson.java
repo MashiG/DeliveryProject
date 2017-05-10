8c1d2282d2f019d534ab5ed4b1a4eefda0ab3d56
ASUS\asus i3 <mekala.rashmika@gmail.com>
2017-05-10 12:21:23 +0530

    add anget 90%

--- app/src/main/java/com/example/hp/deliveryproject/ManagerAddDeliveryPerson.java
+++ app/src/main/java/com/example/hp/deliveryproject/ManagerAddDeliveryPerson.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +16,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import Model.User;
 
 /**
  * Created by murtaza on 5/10/17.
@@ -46,7 +49,19 @@ public class ManagerAddDeliveryPerson extends AppCompatActivity {
                 rootRef.addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.child("users").hasChild(et_email.getText().toString())){
+                            Snackbar mySnackbar = Snackbar.make(getWindow().getDecorView().findViewById(android.R.id.content), "Agent Already Available", Snackbar.LENGTH_SHORT);
+                            mySnackbar.show();
+                        }else{
+                            User userReg =  new User(et_name.getText().toString(),et_password.getText().toString(),true,et_email.getText().toString(),et_phone.getText().toString(), "agent".toString());
+                            FirebaseDatabase database = FirebaseDatabase.getInstance();
+                            DatabaseReference myRef = database.getReference("tables");
 
+                            myRef = myRef.child("users");
+                            myRef = myRef.child(userReg.getEmail().toString());
+                            myRef.setValue(userReg);
+                            finish();//mekala
+                        }
                     }
 
                     @Override

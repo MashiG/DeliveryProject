5ca3fe0dd2c7b1fff046ca752a73d59d3be3df24
ASUS\asus i3 <mekala.rashmika@gmail.com>
2017-05-10 11:33:54 +0530

    add new agent partial

--- app/src/main/java/com/example/hp/deliveryproject/ManagerAddDeliveryPerson.java
+++ app/src/main/java/com/example/hp/deliveryproject/ManagerAddDeliveryPerson.java
@@ -1,8 +1,47 @@
 package com.example.hp.deliveryproject;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 /**
  * Created by murtaza on 5/10/17.
  */
 
-public class ManagerAddDeliveryPerson {
+public class ManagerAddDeliveryPerson extends AppCompatActivity {
+
+    private EditText et_name, et_email, et_password, et_phone  ;
+    private String name,Email,password,phone;
+    Button regbtn;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        et_name=(EditText) findViewById(R.id.name);
+        et_email=(EditText)findViewById(R.id.Email);
+        et_password=(EditText) findViewById(R.id.Password);
+        et_phone=(EditText) findViewById(R.id.phone);
+        regbtn=(Button) findViewById(R.id.registbtn);
+
+        regbtn.setOnClickListener(new View.OnClickListener(){
+            @RequiresApi(api = Build.VERSION_CODES.ECLAIR)
+            @Override
+            public void onClick(View v){
+                overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference rootRef =database.getReference("tables");
+            }
+        });
+
+    }
 }

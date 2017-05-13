a8ef3aef0e0f3682d2ade3f200fce25875597c19
lalendra94 <kasun.silva94@yahoo.com>
2017-05-13 18:08:28 +0530

    delivery agent complete button

--- app/src/main/java/Controller/DeliveryAgentViewPickupListController.java
+++ app/src/main/java/Controller/DeliveryAgentViewPickupListController.java
@@ -1,5 +1,7 @@
 package Controller;
 
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,12 +9,18 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.example.hp.deliveryproject.DeliveryAgentDashboard;
+import com.example.hp.deliveryproject.DeliveryAgentViewPickup;
 import com.example.hp.deliveryproject.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.List;
 
 import Model.DeliveryDetails;
 import Model.User;
+
+
 
 /**
  * Created by Bhagya Rathnayake on 5/10/2017.
@@ -55,7 +63,13 @@ public class DeliveryAgentViewPickupListController extends RecyclerView.Adapter<
         holder.btnAgentAccept.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.println("AGENT ACCEPT THIS ID"+deliveryAgent.getDeliveryID());
+                String id=deliveryAgent.getDeliveryID().toString();
+                System.out.println("AGENT ACCEPT THIS ID "+deliveryAgent.getDeliveryID());
+                FirebaseDatabase chkDB = FirebaseDatabase.getInstance();
+                DatabaseReference chkref = chkDB.getReference("tables/deliverydetails");
+                chkref.child(id).child("status").setValue("Complete");
+               
+
             }
         });
 
